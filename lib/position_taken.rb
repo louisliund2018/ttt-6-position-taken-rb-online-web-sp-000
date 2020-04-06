18795b4a6c214242e275a731ca3d1c8e1375e5fe
Louis Liu <louis.liu2013@gmail.com>
2020-04-06 01:02:26 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
-# code your #position_taken? method here!
+def position_taken?(board, index)
+  if board(" ")
+    puts "false"
+end
+  if board("")
+    puts "false"
+end
+  if board()
+    puts "false"
+  end
+end
