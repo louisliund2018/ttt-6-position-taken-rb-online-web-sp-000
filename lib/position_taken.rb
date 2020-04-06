43b3338832856d1734dc4b300cb707aff8af19ef
Louis Liu <louis.liu2013@gmail.com>
2020-04-06 01:31:40 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,5 +3,9 @@ def position_taken?(board, index)
     false
   elsif (board[index] == "")
     false
+  elsif (board[index] == nil)
+    false
+  else (board[index] == "X" || "O")
+    true
   end
 end
